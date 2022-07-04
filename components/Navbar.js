613f351be2a80f64541daa618588f482e2d27ac6
Kashif Nazar <muhammad.kashif.nazar@gmail.com>
2022-07-04 16:07:31 +0100

    Added ninjas logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 
 const Navbar = () => {
     return ( <nav>
         <div className="logo">
-            <Link href="/"><h1>Ninja List</h1></Link>
+            <Link href="/"><Image src="/logo.png" width={128} height={77} /></Link>
         </div>
         <Link href="/"><a>Home</a></Link>
         <Link href="/about"><a>About</a></Link>
